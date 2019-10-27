170801abd2f3fe2fa21bb8339f4764e4c8cd85e9
luhanisbae <freakfreak1010@gmail.com>
2019-10-27 20:44:31 +0100

    Hjælp

--- Presentation layer.py
+++ Presentation layer.py
@@ -24,21 +24,32 @@ class BookingSystem:
         # reservationsframe
         self._res_frame = Frame(self._root)
 
+        # foto på forside
+        self._photo_front = PhotoImage(file='forside.png')
+
+        # overskrift
+        self._headline_label = Label(self._main_frame, text='BOOK EN FRISØRTID', font=('Helvetica 35 bold'))
+
         # knapper
-        self._opret_bruger_btn = Button(self._main_frame, text='Opret bruger')
-        self._log_ind_bruger_btn = Button(self._main_frame, text='Log ind')
+        self._opret_bruger_btn = Button(self._main_frame, text='Opret bruger', font=('Helvetica', 20))
+        self._log_ind_bruger_btn = Button(self._main_frame, text='Log ind', font=('Helvetica', 20))
+
         self._brugernavn_label = Label(self._user_frame, text='Brugernavn:')
         self._brugernavn_entry = Entry(self._user_frame)
         self._kode_label = Label(self._user_frame, text='Adgangskode:')
         self._kode_entry = Entry(self._user_frame)
+        self._photo_front_label = Label(self._main_frame, image=self._photo_front)
 
         # pack
+        self._headline_label.pack(side=TOP)
         self._main_frame.pack(side=TOP)
         self._user_frame.pack(side=BOTTOM)
         self._res_frame.pack(side=BOTTOM)
 
-        self._opret_bruger_btn.pack(side=LEFT)
-        self._log_ind_bruger_btn.pack(side=LEFT)
+        self._opret_bruger_btn.pack(side=BOTTOM)
+        self._log_ind_bruger_btn.pack(side=BOTTOM)
+
+        self._photo_front_label.pack()
 
         self._root.geometry('500x500')
 
