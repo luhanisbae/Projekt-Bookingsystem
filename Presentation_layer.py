e2160041227b5b32709c9ccb0584673ba4e3cbe7
luhanisbae <freakfreak1010@gmail.com>
2019-11-11 14:07:59 +0100

    .

--- Presentation_layer.py
+++ Presentation_layer.py
@@ -17,7 +17,6 @@ class BookingSystem:
         self._root = root
         self._use = UsernameAndPass()
 
-
         # forside frame
         self._main_frame = Frame(self._root)
 
@@ -34,7 +33,7 @@ class BookingSystem:
         self._headline_label = Label(self._main_frame, text='BOOK EN FRISØRTID', font=('Helvetica 35 bold'))
 
         # knapper
-        self._opret_bruger_btn = Button(self._main_frame, text='Opret bruger', font=('Helvetica', 20))
+        self._opret_bruger_btn = Button(self._main_frame, text='Opret bruger', font=('Helvetica', 20), command=self.switch)
         self._log_ind_bruger_btn = Button(self._main_frame, text='Log ind', font=('Helvetica', 20))
 
         self._brugernavn_label = Label(self._user_frame, text='Brugernavn:')
@@ -46,16 +45,19 @@ class BookingSystem:
         # pack
         self._headline_label.pack(side=TOP)
         self._main_frame.pack(side=TOP)
-        self._user_frame.pack(side=BOTTOM)
-        self._res_frame.pack(side=BOTTOM)
 
-        self._opret_bruger_btn.pack(side=BOTTOM, command=self._use.create_user(self._brugernavn_entry, self._kode_entry))
+        self._opret_bruger_btn.pack(side=BOTTOM)
         self._log_ind_bruger_btn.pack(side=BOTTOM)
 
         self._photo_front_label.pack()
 
         self._root.geometry('500x500')
 
+    def switch(self):
+        self._main_frame.forget()
+        self._user_frame.pack(side=TOP)
+
+#self._use.create_user(self._brugernavn_entry, self._kode_entry)
 
 if __name__ == "__main__":
     window = Tk()
