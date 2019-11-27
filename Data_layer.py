717e48f252bbaf968dc0bb1a49a2fda1843c8faa
luhanisbae <freakfreak1010@gmail.com>
2019-11-27 12:00:06 +0100

    3

--- Data_layer.py
+++ Data_layer.py
@@ -24,17 +24,15 @@ class UsernameAndPass:
             return True
 
     def create_user(self, user, password):
-        self._userspass[user] = password
-        #user = self._str_to_class("User_obj")
-        self._users.append(user)
-        print(self._users)
-        #for i in self._users:
-        #    print(i)
+        if not user in self._users:
+            self._userspass[user] = password
+            #user = self._str_to_class("User_obj")
+            self._users.append(user)
+            print(self._users)
+            #for i in self._users:
+            #    print(i)
 
     def delete_user(self, user):
         self._userspass.pop(user)
         self._users.remove(user)
-
-    def edit_user(self, user, new_password):
-        self._userspass[user] = new_password
 
--- Presentation_layer.py
+++ Presentation_layer.py
@@ -48,6 +48,7 @@ class BookingSystem:
         self._pass_label = Label(self._user_frame, text='Adgangskode:')
         self._pass_entry = Entry(self._user_frame, show='*')
 
+        self._text1_label = Label(self._user_frame, text='Brugernavnet findes allerede. Vælg et andet.', fg='red')
         self._back1_btn = Button(self._user_frame, text='Back', font=('Helvetica', 8), command=self.switch_to_main)
 
         # pack
@@ -82,12 +83,7 @@ class BookingSystem:
 
         self._back2_btn = Button(self._res_frame, text='Back', font=('Helvetica', 8), command=self.switch_to_user)
 
-        self._text1_label = Label(self._res_frame, text='OBS! Tjek, hvad du har indtastet.', font=('Helvetica', 15), fg='red')
-
-        self._new_password_label = Label(self._res_frame, text="Nyt password", font=('Helvetica', 10))
-        self._new_password_entry = Entry(self._res_frame, font=('Helvetica', 10))
-        self._new_password_btn = Button(self._res_frame, text="Skift", font=('Helvetica', 10), command=self._edit_password)
-
+        self._text2_label = Label(self._res_frame, text='OBS! Tjek, hvad du har indtastet.', font=('Helvetica', 15), fg='red')
 
         # listbox
         self._res_lb = Listbox(self._res_frame)
@@ -100,13 +96,13 @@ class BookingSystem:
         self._del_res_btn.pack()
         self._edit_res_btn.pack()
         self._back2_btn.pack(side=BOTTOM)
-        self._edit_password_btn.pack(side=BOTTOM)
 
         self._show_interval_res_btn.pack()
 
         self._root.geometry('500x500')
 
 
+    # funktioner til objekterne
     def switch_from_main(self):
         self._main_frame.forget()
         self._user_frame.pack()
@@ -119,6 +115,8 @@ class BookingSystem:
         if self._use.login_user(user=self._username_entry.get(), password=self._pass_entry.get()) is True:
             self._user_frame.forget()
             self._res_frame.pack()
+        else:
+            self._text1_label.pack(side=BOTTOM)
 
     def switch_to_user(self):
         self._res_frame.forget()
@@ -142,10 +140,10 @@ class BookingSystem:
         pass
 
     def add_to_listbox(self):
-        if len(self._res_entry.get()) != 4 or int(self._res_entry.get()) in self._res._reservation:
-            self._text1_label.pack(side=BOTTOM)
+        if len(self._res_entry.get()) != 4 or 700 >= int(self._res_entry.get()) or int(self._res_entry.get()) >= 1600 or int(self._res_entry.get()) in self._res._reservation:
+            self._text2_label.pack(side=BOTTOM)
         else:
-            self._text1_label.forget()
+            self._text2_label.forget()
             self._res_lb.insert(END, int(self._res_entry.get()))
             self._res.create_reservation(int(self._res_entry.get()))
 
@@ -154,7 +152,7 @@ class BookingSystem:
         self._res.delete_reservation(int(self._res_lb.curselection()))
 
     def edit_in_listbox(self):
-        if len(self._res_entry.get()) != 4 or int(self._res_entry.get()) in self._res._reservation:
+        if len(self._res_entry.get()) != 4 or 700 >= int(self._res_entry.get()) or int(self._res_entry.get()) >= 1600 or int(self._res_entry.get()) in self._res._reservation:
             pass
         else:
             self._res_lb.insert(self._res_lb.curselection(), int(self._res_entry.get()))
