1e08379a27c39247f76b6ffde850fb8093008438
SplatterDK <thomas0876@hotmail.com>
2019-11-27 09:52:08 +0100

    k

--- Data_layer.py
+++ Data_layer.py
@@ -21,17 +21,12 @@ class UsernameAndPass:
 
     def login_user(self, user, password):
         if self._userspass[user] == password:
-            for i in range(len(self._users)):
-                print(self._users[i])
             return True
 
     def create_user(self, user, password):
         self._userspass[user] = password
         usererl = User_obj()
         self._users.append(usererl)
-        print(self._users)
-        for i in range(len(self._users)):
-            print(self._users[i])
 
     def delete_user(self, user):
         self._userspass.pop(user)
--- Presentation_layer.py
+++ Presentation_layer.py
@@ -149,7 +149,6 @@ class BookingSystem:
             self._text1_label.forget()
             self._res_lb.insert(END, int(self._res_entry.get()))
             self._res.create_reservation(int(self._res_entry.get()))
-            print(self._res._reservation)
 
     def delete_in_listbox(self):
         self._res_lb.delete(self._res_lb.curselection())
